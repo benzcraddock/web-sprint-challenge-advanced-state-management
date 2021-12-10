3c60d0e5b7e3242afda22cff844ad531688d259d
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-10 16:02:49 -0500

    complete app.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,16 @@ import AddForm from './components/AddForm';
 import SmurfList from './components/SmurfList';
 import Header from './components/Header';
 
+// connect fetchSmurfs actions to App and import connect
+import { fetchSmurfs } from "./actions";
+import { connect } from "react-redux";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 
 const App = ()=> {
+  // const { smurfs, isLoading, error } = props;
+
   return (
     <div className="App">
       <Header />
@@ -20,7 +26,15 @@ const App = ()=> {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return ({
+    smurfs: state.smurfs,
+    isLoading: state.isLoading,
+    error: state.error
+  })
+}
+
+export default connect(mapStateToProps, { fetchSmurfs })(App);
 
 //Task List:
 //1. Connect the fetchSmurfs actions to the App component.
