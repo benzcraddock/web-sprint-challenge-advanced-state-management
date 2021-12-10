f3966dfeeb87b0448443c59160921aeb99e08e49
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-12-10 14:47:26 -0500

    complete index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Add in all necessary components and libary methods:
+// import createStore, thunk and applyMiddleware
+import { createStore, applyMiddleware } from "redux";
+import { logger } from "redux-logger";
+import { thunk } from "redux-thunk";
+
+// import Provider and reducer
+import { Provider } from "react";
+import { reducer } from "./reducers";
+
 import "./index.css";
 import App from "./App";
+import { Form } from "react-bootstrap";
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
+// Create a store that includes thunk and logger middleware support.
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
 const rootElement = document.getElementById("root");
 
+// Wrap App with Provider
 ReactDOM.render(
-    <App />, 
+    <Provider store={store}>
+        <App />
+    </Provider>, 
     rootElement
 );
 
